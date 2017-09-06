720aa03a6ba307a9024916a76b535e74a6db860c
T-rav <tmfrisinger@gmail.com>
2017-09-06 12:42:44 +0200

    Removed unused packages

--- source/TddBuddy.EntityFramework.Utils.Tests/Migrations/Configuration.cs
+++ source/TddBuddy.EntityFramework.Utils.Tests/Migrations/Configuration.cs
@@ -1,6 +1,5 @@
 ﻿using System.Data.Entity.Migrations;
 using EntityFramework.Utils.Tests.Context;
-using EntityFramework.Utils.Tests.ExampleDb;
 
 namespace EntityFramework.Utils.Tests.Migrations
 {
